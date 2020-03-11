505bfa3392abb612f83a9c2c17df69d47fa1515b
Yasser Nascimento <yassern@protonmail.com>
2020-03-11 16:27:16 -0300

    utils one lesson

---
+++ src/utils-one.ts
@@ -0,0 +1,28 @@
+// lesson two of egghead's ramda course
+import { pipe, tail, split, map, fromPairs } from 'ramda'
+
+const queryString = '?page=2&pageSize=10&total=203'
+
+const expectedResult = {
+  page: '2',
+  pageSize: '10',
+  total: '203'
+}
+
+const stdParseQs = (query: string) => {
+  const firstCharRemoved = query.slice(1)
+  const firstSplit = firstCharRemoved.split('&')
+  const pairs = firstSplit.map(piece => piece.split('='))
+  const objectsFromArrays = pairs.map(p => ({ [p[0]]: p[1] }))
+
+  const flattenArrayOfObjects = objectsFromArrays.reduce(
+    (acc, curr) => ({ ...acc, ...curr }),
+    {}
+  )
+  return flattenArrayOfObjects
+}
+
+const fnParseQs = pipe(tail, split('&'), map(split('=')), fromPairs)
+
+test('std way', () => expect(stdParseQs(queryString)).toEqual(expectedResult))
+test('fn way', () => expect(fnParseQs(queryString)).toEqual(expectedResult))
