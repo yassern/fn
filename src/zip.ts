df7be8ced2bc13d77dd491e6e8e257420a6f61a4
Yasser Nascimento <yassern@protonmail.com>
2020-03-13 12:14:16 -0300

    zip lesson

---
+++ src/zip.ts
@@ -0,0 +1,29 @@
+// lesson fourteen of egghead's ramda course
+import { zipObj } from 'ramda'
+
+const getName = () => Promise.resolve('Andrew')
+const getHobbies = () =>
+  new Promise((res, rej) => res(['kayaking', 'woodworking', 'screencasting']))
+
+const promise = Promise.all([getName(), getHobbies()])
+
+const expectedResult = {
+  name: 'Andrew',
+  hobbies: ['kayaking', 'woodworking', 'screencasting']
+}
+
+const stdConvertToObj = async promise => {
+  const [name, hobbies] = await promise
+  return { name, hobbies }
+}
+
+const fnConvertToObj = async promise =>
+  zipObj(['name', 'hobbies'], await promise)
+
+test('std way', async () => {
+  expect(await stdConvertToObj(promise)).toEqual(expectedResult)
+})
+
+test('fn way', async () => {
+  expect(await fnConvertToObj(promise)).toEqual(expectedResult)
+})
